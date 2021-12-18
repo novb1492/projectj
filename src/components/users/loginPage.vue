390267408148d15e527ead5a47a401a592db4506
novb1492 <novb1492@naver.com>
2021-12-18 23:06:36 +0900

    로그인구현시작

--- src/components/users/loginPage.vue
+++ src/components/users/loginPage.vue
@@ -2,10 +2,10 @@
   <div class="loginPage givecenter">
       <short-logo/>
       <div class="mb-2">이메일</div>
-      <input type="text" class="mb-3" placeholder=" 이메일을 입력해주세요">
+      <input type="text" class="mb-3" id="email" placeholder=" 이메일을 입력해주세요">
       <div class="mb-2">비밀번호</div>
-      <input type="password">
-      <input type="button" value="로그인">
+      <input type="password" id="pwd">
+      <input type="button" @click="tryLogin" value="로그인">
       <br>
       <input type="button" class="mb-3 mt-3 mr20" value="네이버로그인"><input type="button" value="카카오로그인">
       <br>
@@ -15,14 +15,26 @@
 <style>
 </style>
 <script>
-import shortLogo from '../layout/shortLogo.vue';
+import * as modules from '../../jslib';
 export default {
-  components: { shortLogo },
   name: 'loginPage',
   data(){
     return{
 
     };
+  },
+  methods:{
+    tryLogin(){
+      var email=modules.getValueById('email');
+      var pwd=modules.getValueById('pwd');
+      let data=JSON.stringify({
+        "email":email,
+        "pwd":pwd,
+      });
+      modules.requestPost("http://localhost:8080/login",data).then(result=>{
+        console.log(result);
+      });
+    }
   },
   
 }
