e5a615c17a2356e6ba0a6009a3dcb576fc96fce4
novb1492 <novb1492@naver.com>
2022-01-01 19:03:58 +0900

    oauth페이지 로그인 결과 구축

--- src/components/resultPage.vue
+++ src/components/resultPage.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="resultPage giveCenter">
-      resultPage
+      <short-logo/>
   </div>
 </template>
 <style>
 </style>
 <script>
+import * as modules from '../jslib';
+import shortLogo from './layout/shortLogo.vue';
 export default {
+  components: { shortLogo },
   name: 'resultPage',
+   data() {
+    return {
+      kind:null,
+      flag:false,
+      action:null,
+      message:null,
+    };
+  },
+  created(){
+    this.action=modules.getParam('action');
+    console.log('action: '+this.action);
+    if(this.action=='login'){
+      console.log("로그인 결과 처리");
+      this.login();
+    }
+  },
+  methods:{
+    login(){
+      this.flag=modules.getParam('result');
+      if(this.flag=='true'){
+        opener.opener.location.reload();
+        opener.close();
+      }else{
+        alert(modules.getParam('message'));
+      }
+      self.close();
+    }
+  },
 }
 </script>
