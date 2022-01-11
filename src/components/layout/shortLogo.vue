dc07f99044c14a6ec446be41033fbdcdf87fa783
kimjunyoung <kimjy80@sesisoft.com>
2022-01-11 16:55:41 +0900

    왜 직접걸지 않으면 text-decoration-line: none이 안먹히는건지..

--- src/components/layout/shortLogo.vue
+++ src/components/layout/shortLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shortLogo giveCenter">
-      <h1> <a  href="/"><p class="pinkText mt-3">JangBoGo</p></a></h1>
+      <h1> <a  href="/" style="text-decoration-line: none;"><p class="pinkText mt-3">JangBoGo</p></a></h1>
       <hr>
   </div>
 </template>
