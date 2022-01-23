d8ab66c1854d30ad79f48707452cf3444ab266cd
novb1492 <novb1492@naver.com>
2022-01-23 14:26:21 +0900

    성공 ec2를 https로 바꿔야되는구나

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ Vue.prototype.$ROLE_ADMIN='ROLE_ADMIN';
 Vue.prototype.$sideVarWitdh=250;//수정시 사이드바 width는 가서 직접 수정해줘야한다
 Vue.prototype.$footerHeigth=50;//수정시 사이드바 width는 가서 직접 수정해줘야한다
 Vue.prototype.$deliverSocketUrl='/ws/deliver';
-Vue.prototype.$shortServerDomain='https://222.237.192.68:8080';//웹소켓통신시 사용
+Vue.prototype.$shortServerDomain='222.237.192.68:8080';//웹소켓통신시 사용
 
 //글로벌 컴포넌츠들
 Vue.component(shortHeader.name, shortHeader); //이렇게 하면 글로벌 컴포넌츠가 된다 
