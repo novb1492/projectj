8000a341c910ccf4cf0d1a627db18971b9ec8684
novb1492 <novb1492@naver.com>
2022-01-23 14:23:57 +0900

    test2

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.use( CKEditor );
 Vue.use(VueDaumPostcode)
 Vue.config.productionTip = false;
 Vue.prototype.$EventBus = new Vue();//컴포넌트간 통신위해
-Vue.prototype.$serverDomain = 'http://ec2-user@ec2-54-180-89-72.ap-northeast-2.compute.amazonaws.com:8080';
+Vue.prototype.$serverDomain = 'https://222.237.192.68:8080';
 Vue.prototype.$domain = 'http://localhost:3030';
 Vue.prototype.$s3Path='https://s3.ap-northeast-2.amazonaws.com';
 Vue.prototype.$ROLE_USER='ROLE_USER';
@@ -21,7 +21,7 @@ Vue.prototype.$ROLE_ADMIN='ROLE_ADMIN';
 Vue.prototype.$sideVarWitdh=250;//수정시 사이드바 width는 가서 직접 수정해줘야한다
 Vue.prototype.$footerHeigth=50;//수정시 사이드바 width는 가서 직접 수정해줘야한다
 Vue.prototype.$deliverSocketUrl='/ws/deliver';
-Vue.prototype.$shortServerDomain='ec2-user@ec2-54-180-89-72.ap-northeast-2.compute.amazonaws.com:8080';//웹소켓통신시 사용
+Vue.prototype.$shortServerDomain='https://222.237.192.68:8080';//웹소켓통신시 사용
 
 //글로벌 컴포넌츠들
 Vue.component(shortHeader.name, shortHeader); //이렇게 하면 글로벌 컴포넌츠가 된다 
