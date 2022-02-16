7e315fa864e7d03f6a84d9331b6c16e2218214b8
kimjunyoung <kimjy80@sesisoft.com>
2022-02-16 14:26:50 +0900

    왜 에이터 컴포넌트가 따로놀지??

--- src/components/companys/flyerDetailPage.vue
+++ src/components/companys/flyerDetailPage.vue
@@ -2,10 +2,23 @@
     <div class="margintopNavSize marginLeftSideSize">
         <div>
          <h3>전단이미지</h3>
-          <span v-for="(product,index) in this.products" :key="index">
-
-            <product-componet :flag="true" :flyerId="flyerId" :storeId="storeId" />
+          <input type="file" id="img" class="mt-2" name="img" accept=".gif, .jpg, .png" @change="uploadAndGetProducts">
+            <br>
+            <img :src="flyerImgPath" id="flyerImg" >
+            <br>
+            <h5>전단고유번호</h5>
+            (업로드시 자동발급)
+            <br>
+            <input type="text" placeholder="업로드시 자동 부여 됩니다" id="flyerId" :value="flyerId" disabled/>
+            {{defaultText}}
+         <ul>
+            <span v-for="(product,index) in products.length" :key="index">
+            <li style="float: left; margin-left: 10px;">
+                <product-componet :flag="true" :flyerId="flyerId" :storeId="storeId" />
+            </li>
           </span>
+         </ul>
+          
       </div>
     </div>
 </template>
@@ -13,7 +26,7 @@
 
 </style>
 <script>
-import { getParam, requestAsyncToGet } from '../../jslib'
+import { getParam, requestAsyncToGet, requestFormAsyncToPost } from '../../jslib'
 import ProductComponet from './productComponet.vue'
 export default {
   name: 'flyerDetailPage',
@@ -23,9 +36,12 @@ export default {
       subSideVarIds:['storeDetailSubSide'],
       storeId:getParam('storeid'),
       products:[],
-      flyer:null,
+      flyerImgPath:null,
       events:null,
       flyerId:0,
+      len:0,
+      text:'',
+      defaultText:'',
     }
   },
   created(){
@@ -33,14 +49,16 @@ export default {
     this.$emit('openSubSide',this.subSideVarIds);
     //새로고침 대응
     this.$emit('changeStoreId',this.storeId);
+  },
+  mounted(){
     requestAsyncToGet(this.$serverDomain+'/auth/store/get/flyer/'+getParam('flyerid')).then(result=>{
         console.log(result);
         if(!result.flyerFlag){
           alert(result.message);
           return;
         }
-        this.flyer=result.flyer;
-        this.flyerId=this.flyer.id;
+        this.flyerImgPath=result.flyer.img_path;
+        this.flyerId=result.flyer.id;
         if(result.productFlag){
           this.products=result.products;
         }
@@ -50,7 +68,31 @@ export default {
     });
   },
   methods:{
+    getSubSideVarIds(){
+      return this.subSideVarIds;
+    },
+    uploadAndGetProducts(){
+      const frm = new FormData();
+      console.log(document.getElementById('img').files[0]);
+      frm.append("upload",document.getElementById('img').files[0]);
+      frm.append("storeId",this.storeId);
+      console.log(frm);
+     this.defaultText='글자를 추출중입니다 시간이 걸리니 페이지를 벗어나지 마세요';
+      requestFormAsyncToPost(this.$serverDomain+'/auth/store/uploadAndGet/'+this.storeId,frm).then(result=>{
+        console.log(result);
+        if(result.flag){
+          document.getElementById('flyerImg').hidden=false;
+          this.imgPath=result.message;
+          this.defaultText='';
+          this.text=result.ocr.message;
+          this.flyerId=result.id;
+          document.getElementById('insertProductArea').hidden=false;
+          return;
+        }
+        alert('파일 업로드에 실패했습니다');
 
+      });
+    },
   },
 }
 </script>
