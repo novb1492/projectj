b92ee5eb0d44729be135e7735f1b694d95add2e4
novb1492 <novb1492@naver.com>
2022-02-28 23:15:57 +0900

    장바구니 테이블 생성

--- src/components/users/showProductDetailPage.vue
+++ src/components/users/showProductDetailPage.vue
@@ -3,7 +3,10 @@
       <ul style="text-align: center;">
         <li><img src="" alt="" id="productImg" srcset=""></li>
         <li id="productName"></li>
-         <li id="price"></li>
+        <li id="origin"></li>
+        <li id="price"></li>
+        <li ><input type="number"  id="count" min="1" value="1" ></li>
+        <li > <input type="button" value="장바구니담기" @click="insertBasket"></li>
       </ul>
   </div>
 </template>
@@ -11,7 +14,7 @@
 
 </style>
 <script>
-import { getParam, requestAsyncToGet } from '../../jslib'
+import { getParam, getValueById, requestAsyncToGet, requestAsyncToPost } from '../../jslib'
 export default {
   name: 'showProductDetailPage',
    data() {
@@ -30,8 +33,20 @@ export default {
         document.getElementById('productImg').src=result.message.productImgPath;
         document.getElementById('productName').innerHTML=result.message.productName;
         document.getElementById('price').innerHTML=result.message.price+'원';
+        document.getElementById('origin').innerHTML='원산지: '+result.message.origin;
         console.log(result);
       });
+    },
+    methods:{
+      insertBasket(){
+        let date=JSON.stringify({
+          "count":getValueById('count'),
+          "productId":this.productId,
+        });
+        requestAsyncToPost(this.$serverDomain+'/auth/user/basket',date).then(result=>{
+          alert(result.message);
+        });
+      },
     },
 }
 </script>
