db45fd4b320b865ce5c315f756436d65b59fcded
novb1492 <novb1492@naver.com>
2022-03-01 13:34:42 +0900

    수량변경 구축

--- src/components/users/basketPage.vue
+++ src/components/users/basketPage.vue
@@ -7,22 +7,25 @@
         <th>가격</th>
         <th>담은날짜</th>
         <tr v-for="(basket,index) in baskets" :key="index"> 
-          <td >{{basket.basket_id}}</td>
+          <td >{{basket.id}}</td>
             <td >
+              <img src="" alt="상품이미지" >
               {{basket.product_name}}
             </td>
-            <td>{{basket.basket_count}}</td>
-            <td>{{basket.basket_count}}</td>
+            <td><input type="number"  :id="basket.id+'count'" :value="basket.basket_count" @change="changeCount(basket.id)"></td>
+            <td><input type="text"  :id="basket.id+'price'" :value="basket.price+'원'" disabled></td>
             <td>{{basket.basket_created}}</td>
         </tr>
     </table>
+    <input type="button" value="선택 구매하기" />
+    <input type="button" value="전체구매하기" />
   </div>
 </template>
 <style>
 
 </style>
 <script>
-import { getParam, requestAsyncToGet } from '../../jslib'
+import { changeValueById, getParam, getValueById, requestAsyncToGet, requestAsyncToPut } from '../../jslib'
 export default {
   name: 'basketPage',
   data() {
@@ -34,6 +37,14 @@ export default {
     this.requestServer();
   },
   methods:{
+    changeCount(id){
+      var count=getValueById(id+'count');
+      requestAsyncToPut(this.$serverDomain+'/auth/user/basket/'+id+'/'+count).then(result=>{
+        if(result.flag){
+          changeValueById(id+'price',result.message+'원');
+        }
+      });
+    },
     requestServer(){
       requestAsyncToGet(this.$serverDomain+'/auth/user/baskets/'+getParam('page')).then(result=>{
         if(!result.flag){
@@ -41,6 +52,7 @@ export default {
           return;
         }
         this.baskets=result.baskets;
+        console.log(this.baskets);
       });
     },
   },
