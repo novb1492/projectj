4c1a2b6d994556787d75dfb2df767c7c5c587e5e
novb1492 <novb1492@naver.com>
2022-03-19 23:29:15 +0900

    주문 내역 페이지 구축시작

--- src/components/users/showPaymentListPage.vue
+++ src/components/users/showPaymentListPage.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-      FDFDF
+      <table>
+          <th>주문번호</th>
+          <th>주문제품이름</th>
+          <th>주문개수</th>
+          <th>쿠폰</th>
+          <th>주문일</th>
+          <th>결제상태</th>
+          <th>결제수단</th>
+        
+      </table>
   </div>
 </template>
 <style>
@@ -10,9 +19,19 @@
 import { requestAsyncToGet } from '../../jslib'
 export default {
   name: 'showPaymentListPage',
+  data() {
+    return {
+      infors:[],
+    }
+  },
   created(){
     requestAsyncToGet(this.$serverDomain+'/auth/user/payments/1/null/null').then(result=>{
         console.log(result);
+        if(result.flag){
+          this.infors=result.message;
+          return;
+        }
+        alert(result.message);
     });
   },
   methods:{
